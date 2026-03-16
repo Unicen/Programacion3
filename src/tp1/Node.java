7a59c0eec5563ed3bb4ac43c747c2a022813309a
Federico Améndola <famendola@alumnos.exa.unicen.edu.ar>
2026-03-16 16:37:55 -0300

    Esqueleto inicial de clase Lista

---
+++ src/tp1/Node.java
@@ -0,0 +1,34 @@
+package ProgramacionIII.tp1;
+
+public class Node<T> {
+
+	private T info;
+	private Node<T> next;
+
+	public Node() {
+		this.info = null;
+		this.next = null;
+	}
+	
+	public Node(T info, Node<T> next) {
+		this.setInfo(info);
+		this.setNext(next);
+	}
+	
+	public Node<T> getNext() {
+		return next;
+	}
+
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+
+	public T getInfo() {
+		return info;
+	}
+
+	public void setInfo(T info) {
+		this.info = info;
+	}
+
+}
